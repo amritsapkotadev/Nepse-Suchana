4d4577031ea2d4e898f5a1621bafdcfc7d7f59b8
Amrit Sapkota <amritsapkota.dev@gmail.com>
2025-12-30 11:54:55 +0545

    slight change

--- backend/src/Routes/PortfolioRoutes.js
+++ backend/src/Routes/PortfolioRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const portfolioController = require('../controller/portfoliocontroller');
+const portfolioController = require('../controller/portfolioController');
 
-// Add stock to portfolio
-router.post('/add', portfolioController.addStock);
+// Create a new portfolio
+router.post('/api/portfolios', portfolioController.createPortfolio);
 
-// Get user's portfolio
-// router.get('/:userId', portfolioController.getPortfolio);
+// Get all portfolios
+router.get('/api/portfolios', portfolioController.getPortfolios);
+
+// Update an existing portfolio
+router.put('/api/portfolios/:id', portfolioController.updatePortfolio);
+
+// Delete a portfolio
+router.delete('/api/portfolios/:id', portfolioController.deletePortfolio);
 
 module.exports = router;
